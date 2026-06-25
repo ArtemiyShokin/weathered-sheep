01a010129a3d228ec5dcd459836524b38419d90c
Artemiy <artemiy.shokin@gmail.com>
2026-06-25 19:17:52 +0200

    changed marker shape

--- components/3DMarker/index.js
+++ components/3DMarker/index.js
@@ -3,9 +3,11 @@ import { useRef } from "react";
 export default function Marker({ position, color }) {
   const meshRef = useRef(null);
   return (
-    <mesh position={position} rotation={[Math.PI, 0, 0]}>
-      <coneGeometry args={[0.07, 0.25]} />
-      <meshStandardMaterial color={color} />
+    <mesh position={position} rotation={[Math.PI, 0, 0]} scale={[1, 1.5, 1]}>
+      {/* <coneGeometry args={[0.07, 0.25]} /> */}
+      <sphereGeometry args={[0.07, 8, 2]} />
+      {/* <meshStandardMaterial color={color} /> */}
+      <meshBasicMaterial color={color} />
     </mesh>
   );
 }
